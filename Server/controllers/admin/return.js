7a2f5f0883f4a2c59652eb1d71238e8a0a4faa89
rithishcodespace <rithishcodespace@gmail.com>
2025-12-29 21:28:17 +0530

    pulling

--- Server/controllers/admin/return.js
+++ Server/controllers/admin/return.js
@@ -1,3 +1,44 @@
 const db = require("../../config/db");
 const createError = require("http-errors");
-const returns = require("../../routes/admin/return");
+
+exports.get_return_requests = (req,res,next) => {
+    try{
+      let sql = "select * from return_requests where return_status = requested";
+      db.query(sql,(error,result) => {
+        if(error)return next(error);
+        if(result.length === 0)return next(createError.NotFound('return requests not found!'));
+
+        res.send(result);
+      })
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+exports.change_status = (req,res,next) => {
+    try{
+      const{id,status} = req.body;
+      if(!id || !id.trim() == ""){
+        return next(createError.BadRequest('Not found!'));
+      }
+
+      if(!['requested','approved','rejected','completed'].includes(status)){
+        return next(createError.BadRequest('Invalid status'));
+      }
+      
+      let sql = "UPDATE return_requests SET return_status = ? WHERE id = ?";
+      db.query(sql,[status,id],(error,result) => {
+        if(error)return next(error);
+        if(result.affectedRows === 0)return next(createError.NotFound('return request not found!'));
+
+        res.send('status updated successfully!');
+      })
+      
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+
