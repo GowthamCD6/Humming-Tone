fbc17146933e94c4f3e130b65d12fd2a4368cfa8
GOWTHAM C D <gowthamnaveen124@gmail.com>
2026-08-12 14:59:42 +0530

    docker and jenkins has created.

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-const mysql = require("mysql2"); // Use the non-promise version
+const mysql = require("mysql2");
 
-// Create a connection pool
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'GCN@6677', 
-  database: 'hummingtone', 
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'GCN@6677',
+  database: process.env.DB_NAME || 'hummingtone',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
